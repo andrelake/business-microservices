c200fa23a0fd20aaf56c8291db48e648781fd1b0
André Lago <andre.lake@hotmail.com>
2020-09-20 20:15:06 -0300

    Gateway config and Proxy service impl

---
+++ proxy-service/src/test/java/br/com/andrelake/proxyservice/ProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrelake.proxyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
